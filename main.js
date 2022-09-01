afc42a78c202d8c1d950c908cc4c06d963f1796b
Bastián Escribano <bastian.escribano@usach.cl>
2022-09-01 17:01:34 -0400

    feat: python-shell integrated

---
+++ main.js
@@ -0,0 +1,24 @@
+let {PythonShell} = require('python-shell')
+
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+PythonShell.run('script.py', null, function (err) {
+  if (err) throw err;
+  console.log('finished');
+});
+
+
+})
+
